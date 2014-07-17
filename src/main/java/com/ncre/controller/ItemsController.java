83452584103c1ac6600c9a4d10334622567bad49
xiaoxinlin <915341372@qq.com>
2014-07-18 01:19:23 +0800

    增加了两级缓存的配置

--- src/main/java/com/ncre/controller/ItemsController.java
+++ src/main/java/com/ncre/controller/ItemsController.java
@@ -2,25 +2,33 @@ package com.ncre.controller;
 
 import java.util.List;
 
+import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.ehcache.CacheInterceptor;
+import com.jfinal.plugin.ehcache.CacheName;
+import com.jfinal.plugin.ehcache.EvictInterceptor;
 import com.ncre.model.TktClass;
 
 public class ItemsController extends Controller implements BaseControllerI<ItemsController>{
 	
 	//获取一套测试题目
+	@Before(CacheInterceptor.class)
+	@CacheName("itemsIndex")
 	public void index(){	
 		List<TktClass> object = TktClass.dao.find("select * from tkt");
 		renderJson(object);
+		return;
 	}
 
 	public void add() {
 		// TODO Auto-generated method stub
 		
 	}
-
+	@Before(EvictInterceptor.class)
+	@CacheName("itemsIndex")
 	public void delete() {
 		// TODO Auto-generated method stub
-		
+		renderText("invoke delete!!");
 	}
 
 	public void query(String sql) {
--- src/main/java/com/ncre/utils/DemoConfig.java
+++ src/main/java/com/ncre/utils/DemoConfig.java
@@ -10,6 +10,7 @@ import com.jfinal.config.Plugins;
 import com.jfinal.config.Routes;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.c3p0.C3p0Plugin;
+import com.jfinal.plugin.ehcache.EhCachePlugin;
 import com.ncre.controller.ItemsController;
 import com.ncre.model.AdminClass;
 import com.ncre.model.AnnouncementClass;
@@ -18,10 +19,11 @@ import com.ncre.model.QuestionClass;
 import com.ncre.model.TktClass;
 import com.ncre.model.XztClass;
 
+
 public class DemoConfig extends JFinalConfig{
 
 	public void configConstant(Constants arg0) {
-		loadPropertyFile("classes/myconfig.txt");
+		loadPropertyFile("myconfig.txt");
 	
 		arg0.setDevMode(true);
 		//arg0.setViewType(ViewType.JSP);
@@ -42,6 +44,7 @@ public class DemoConfig extends JFinalConfig{
 		C3p0Plugin c3p0Plugin = new C3p0Plugin(getProperty("jdbcUrl"), getProperty("user"), getProperty("password"));
 		arg0.add(c3p0Plugin);
 		ActiveRecordPlugin arp = new ActiveRecordPlugin(c3p0Plugin);
+		arp.setShowSql(true);
 		arg0.add(arp);
 		
 		//增加表和对象之间的映射，第一个参数为所对应的表名，第二个参数是表所对应的Class类
@@ -51,6 +54,9 @@ public class DemoConfig extends JFinalConfig{
 		arp.addMapping("question", QuestionClass.class);
 		arp.addMapping("tkt", TktClass.class);
 		arp.addMapping("xzt", XztClass.class);
+		
+		//配置二级缓存
+		arg0.add(new EhCachePlugin());
 	}
 
 	@Override
